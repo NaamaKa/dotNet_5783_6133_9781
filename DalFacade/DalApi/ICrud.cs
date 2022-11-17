d934e63e216ef51361ff4027a5928260ed0f4c9b
GilaMiriam <gilamkuber@gmail.com>
2022-11-17 15:51:22 +0200

    123

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -8,6 +8,10 @@ namespace DalApi
 {
     public interface ICrud<T>
     {
-       
+       public void Add(T entity);
+       public void Update(T entity);
+       public void Delete(T entity);
+       public T Get(int id);
+       public IEnumerable<T> GetAll();
     }
 }
