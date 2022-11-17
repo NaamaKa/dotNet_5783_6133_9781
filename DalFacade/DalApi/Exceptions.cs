d829704dfd6c5da89e5c957291b16f2031316aee
GilaMiriam <gilamkuber@gmail.com>
2022-11-17 17:17:31 +0200

    123

---
+++ DalFacade/DalApi/Exceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+  public class NotFound : Exception
+    {
+        public NotFound():base("not found") { }
+      
+        public NotFound(string s) : base("could notb"+s+" sd")
+        {
+
+        }
+    }
+}
---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    internal interface IDal
+    {
+        IProduct public product {get;}
+        IOrder public order {get;}
+        IOrderItem public orderitem{ get; }
+    }
+}
