b35dddc3793d673a19f76cd5379313c8bedd2ff0
GilaMiriamKuber <gilamkuber@gmail.com>
2023-03-04 22:01:28 +0200

    more

--- PL/windows/ProductForList.xaml.cs
+++ PL/windows/ProductForList.xaml.cs
@@ -85,6 +85,7 @@ public partial class ProductForList : Window
     private void Back_Button_Click_1(object sender, RoutedEventArgs e)
     {
           new MainWindow().Show();
+        Close();
     }
 
     private void chkEnable_Click(object sender, RoutedEventArgs e)
